26c3ac142a7c8b09f3e9914562a8d6d2fa792ca4
AAYUS KARKI <alexkarki2060@gmail.com>
2024-04-25 17:52:23 +0545

    Socket integration going on

--- FRONTEND/src/components/home/home.jsx
+++ FRONTEND/src/components/home/home.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import {useEffect, useState}from 'react';
 import Navbar from '../navbar/navbar';
 import Image from '../home/aayus.jpg';
 import { useSelector } from 'react-redux';
+import io from 'socket.io-client';
 function Home() {
   const authuser = useSelector((store) => store.user.authuser)
+
+  const[message, setMessage] = useState('')
+
+  const [ServerMessage, setServerMessage] = useState('')
+
+  useEffect(() => {
+    const socket = io.connect('http://localhost:8000');
+
+    // Function to emit message when component mounts
+    const sendMessage = () => {
+      socket.emit('message', message);
+    };
+
+    // Emit message when component mounts
+    sendMessage();
+
+    socket.on('message', (data) => {
+      console.log(data);
+      setServerMessage(data);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Prevent default form submission behavior
+    const socket = io.connect('http://localhost:8000');
+    socket.emit('message', message);
+  };
   return (
     <>
      <div style={{border: '18px solid #4a5568', borderRadius: '21px'}}>
@@ -18,6 +50,15 @@ function Home() {
         </div> 
         </div>
       </div> 
+
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="message">Message</label>
+          <input type="text" id="message" value={message} onChange={(e) => setMessage(e.target.value)} />
+          <button type="submit">Send</button> {/* Add a submit button */}
+        </form>
+      </div>
+      {ServerMessage && <div>{ServerMessage}</div>}
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,48 @@
 import {app} from "./app.js";
+import { Server } from "socket.io";
 import dotenv from "dotenv";
+import http from "http";
 import connectDB from './dbconfig/index.js';
 
 dotenv.config({ path: "./.env" });
 
 const port = process.env.PORT||3000;
 
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+})
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("message", (data) => {
+    console.log(data);
+    io.emit("message",data);
+  })
+
+
+  socket.on("typing", (data) => {
+    console.log(data);
+  })
+
+
+
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
 
 
 
 connectDB().then(() => {
-  app.listen(port, () => {
+  server.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
   });
 })
